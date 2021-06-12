4697daa8a861c2ed2d5daaea488cf8f4b0af0680
author <author@fourf>
2021-06-12 21:33:49 +0800

    feat: add some cards

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Divider } from 'antd';
+import { Divider, Card, Row, Col } from 'antd';
 import { PageContainer, BasicLayoutProps } from '@ant-design/pro-layout';
 import useStore, { APIS, oauth } from '@/pages/profile/store';
+import { Link } from 'umi';
 
 const txt = `
 ## QTrade-V2
@@ -32,10 +33,55 @@ export default (p: BasicLayoutProps) => {
     </div>
   );
 
+  const services = (
+    <div>
+      <Divider orientation="left">Data</Divider>
+      <Row>
+        <Col span="6">
+          <Card title="AKShare">
+            <Link to="/akshare/intro">AKShare</Link>
+            <p>AKShare introduction</p>
+          </Card>
+        </Col>
+      </Row>
+      <Divider orientation="left">Strategy</Divider>
+      <Row>
+        <Col span="6">
+          <Card title="QDII Premium">
+            <Link to="/strategy/qdii-premium">QDII Premium</Link>
+            <p>QDII Premium</p>
+          </Card>
+        </Col>
+      </Row>
+      <Divider orientation="left">Tutorial</Divider>
+      <Row>
+        <Col span="6">
+          <Card title="美股">
+            <Link to="/tutorial/stock-us">美股</Link>
+            <p>美股相关教程</p>
+          </Card>
+        </Col>
+        <Col span="6" offset="2">
+          <Card title="期权">
+            <Link to="/tutorial/stock-us">美股</Link>
+            <p>期权相关教程</p>
+          </Card>
+        </Col>
+        <Col span="6" offset="2">
+          <Card title="宏观经济学">
+            <Link to="/tutorial/stock-us">宏观经济学</Link>
+            <p>宏观经济学</p>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+
   return (
     <PageContainer>
       <ReactMarkdown>{txt}</ReactMarkdown>
       {store.user?.login ? '' : dom}
+      {services}
     </PageContainer>
   );
 };
